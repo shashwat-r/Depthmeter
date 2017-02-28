d9796ad75de9b2e341d6efc32f2ec0373fbf7e74
Rahul Choudhary <rahulc.0212@gmail.com>
2017-02-28 23:31:51 +0530

    Create water_level.ino
    Code for water level measurement.

---
+++ iteration/first/water_level.ino
@@ -0,0 +1,42 @@
+#include <math.h>
+const int led = 13;
+const int trig = 9;
+const int echo = 10;
+float duration;
+float distance;
+float error;
+int count = 100;
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  int blinksps=10;
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  
+  duration = pulseIn(echo, HIGH);
+  distance= duration*0.034/2;
+  error=0.00882724*distance+(0.981084);
+  distance+=error;
+  Serial.print("Distance:");
+  Serial.println(distance);
+  if(distance<=10){
+    for(int i;i<blinksps;i++){
+      digitalWrite(led, HIGH);
+      delay(50);
+      digitalWrite(led, LOW);
+      delay(50);
+    }
+  }
+  else{
+     delay(1000);
+  }
+  //count--;
+  //if(count==0)exit(0);
+}
